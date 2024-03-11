bbf12c88d78768d89c6fe1cba89a7bdc617ef45e
luciel53 <l.leroty@gmail.com>
2024-03-12 00:47:28 +0100

    task 6

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,6 +1,7 @@
 List<double> convertToF(List<double> temperaturesInC) {
 	return temperaturesInC.map((celsius) {
 		double fahrenheit = (celsius * 9 / 5) + 32;
-		return fahrenheit.roundToDouble() * 0.01;
+		String Roundedfahrenheit = fahrenheit.toStringAsFixed(2);
+		return double.parse(Roundedfahrenheit);
 	}).toList();
 }
